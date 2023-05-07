0802fb45dcdcf6c7a958336948f12f2d1140e960
personal <edgaras.valincius@qa.com>
2023-05-07 09:42:28 +0100

    trigger build

---
+++ src/contracts/Variables.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Variables {
+    uint public unlockTime;
+    address payable public owner;
+    string public randomString;
+
+    constructor(uint _unlockTime) payable {
+        require(
+            block.timestamp < _unlockTime,
+            "Unlock time should be in the future"
+        );
+
+        unlockTime = _unlockTime;
+        owner = payable(msg.sender);
+        randomString = "Hello World";
+    }
+}
